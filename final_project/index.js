a393829b11602712b9d4127633078f737ad6e624
Denys Kondratiuk <denys.kondratiuk@globalpay.com>
2024-12-03 23:37:03 -0500

    Implementing Getter for the list of books available

--- final_project/index.js
+++ final_project/index.js
@@ -11,10 +11,23 @@ app.use(express.json());
 app.use("/customer",session({secret:"fingerprint_customer",resave: true, saveUninitialized: true}))
 
 app.use("/customer/auth/*", function auth(req,res,next){
-//Write the authenication mechanism here
+const token = req.headers['x-access-token'];
+    if(!token){
+        return res.status(401).json({message: "Access denied! Token not provided"});
+    }
+    jwt.verify(token, "fingerprint_customer", (err, user) => {
+        if(err){
+            return res.status(403).json({message: "Access denied! Invalid token"});
+        }
+        req.user = user;
+        next();
+    });
+
+
 });
- 
-const PORT =5000;
+
+
+const PORT =5001;
 
 app.use("/customer", customer_routes);
 app.use("/", genl_routes);
--- final_project/router/general.js
+++ final_project/router/general.js
@@ -12,8 +12,13 @@ public_users.post("/register", (req,res) => {
 
 // Get the book list available in the shop
 public_users.get('/',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+
+    let booksList = [];
+    for (let book in books) {
+        booksList.push(books[book]);
+    }
+    return res.status(200).json(booksList);
+
 });
 
 // Get book details based on ISBN
@@ -21,7 +26,7 @@ public_users.get('/isbn/:isbn',function (req, res) {
   //Write your code here
   return res.status(300).json({message: "Yet to be implemented"});
  });
-  
+
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
   //Write your code here
